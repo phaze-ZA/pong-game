1e10ca31cbd60e9d08ac93edfb7cb114047cc163
Luke Swan <swan.luke9@gmail.com>
2022-10-16 02:39:26 +0300

    Adding interface for game object

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+import { Point } from "pixi.js";
+
+export interface IGameObject {
+    velocity: Point;
+    name: string;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    centerX?: number;
+    centerY?: number;
+    halfWidth?: number;
+    halfHeight?: number;
+
+    update(delta: number): void;
+}
